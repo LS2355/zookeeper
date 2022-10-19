d08c0ddad89ef3c2f402aa9b1237650412b7c7c3
Luis S <Luissalcedo2355@hotmail.com>
2022-10-18 21:38:50 -0700

    MVP

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
@@ -47,6 +48,14 @@ function filterByQuery(query, animalsArray) {
 }
 
 
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+};
+
+
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -54,6 +63,17 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 
 
 app.listen(PORT, () => {
